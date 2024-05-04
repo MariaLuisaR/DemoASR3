bbdb8b5fbf3f65f3ec57675f4fc10c71e078b0e0
MariaLuisaR <98471527+MariaLuisaR@users.noreply.github.com>
2024-05-04 11:46:14 -0500

    Delete DemoASR2-main/document_update_project directory

--- DemoASR2-main/document_update_project/document_update/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.upload_file, name='upload_file'),  
-    path('process_email/', views.process_email, name='process_email')
-
-]
